2a29bd2add3860a5f98924950fe5dd63a020c193
ViniciusMdJ <74380890+ViniciusMdJ@users.noreply.github.com>
2023-01-25 11:57:00 -0300

    change files

--- index.js
+++ index.js
@@ -1,448 +1,54 @@
-let TuringContract;
+let SelectiveContract;
+
+const Selective_Contract_Address = "0xB26834d7180b6b16Eda20D69e85A5f7e4b90E190";
 
 
-const divMainInteraction = document.querySelector("#contractInteraction");
-const divMainGeneration = document.querySelector("#contractGenerator");
+const Selective_Contract_ABI = [];
+const Collector_Contract_ABI = [];
 
-const glassvalue = document.querySelector("#glassvalue") ;
-const metalvalue = document.querySelector("#metalvalue");
-const papervalue = document.querySelector("#papervalue");
-const plasticvalue = document.querySelector("#plasticvalue");
-const btnGenerat = document.querySelector("#generat");
+// const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
+// provider.send("eth_requestAccounts", []).then(() => {
+//   provider.listAccounts().then((accounts) => {
+//     const signer = provider.getSigner(accounts[0]);
+    
+//     /* 3.1 Create instance of Selective smart contract */
+//     SelectiveContract = new ethers.Contract(
+//       Selective_Contract_Address,
+//       Selective_Contract_ABI,
+//       signer
+//     );
+//     const Collector_Contract_Address = SelectiveContract.getCollectorAddress();
+//     const CollectorContract = new ethers.Contract(
+//       Collector_Contract_Address,
+//       Collector_Contract_ABI,
+//       signer
+//     );
+    
+    
+//     });
+// });
+  
+  const divMainInteraction = document.querySelector("#contractInteraction");
+  const divMainGeneration = document.querySelector("#contractGenerator");
+  
+  divMainGeneration.style.display = 'none';
 
-const btnCreatContract = document.querySelector("#factory");
-const btnInteractionContract = document.querySelector("#interaction");
-
-
-
-
-divMainGeneration.style.display = 'none';
-
-// 2. Set contract address and ABI
-const Turing_Contract_Address = "0xB26834d7180b6b16Eda20D69e85A5f7e4b90E190";
-
-
-const Turing_Contract_ABI = [
-	{
-		"inputs": [],
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "owner",
-				"type": "address"
-			},
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "spender",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "value",
-				"type": "uint256"
-			}
-		],
-		"name": "Approval",
-		"type": "event"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "spender",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "approve",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "spender",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "subtractedValue",
-				"type": "uint256"
-			}
-		],
-		"name": "decreaseAllowance",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "endVoting",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "spender",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "addedValue",
-				"type": "uint256"
-			}
-		],
-		"name": "increaseAllowance",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "receptor",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "saTurings",
-				"type": "uint256"
-			}
-		],
-		"name": "issueToken",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "to",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "transfer",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "from",
-				"type": "address"
-			},
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "to",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "value",
-				"type": "uint256"
-			}
-		],
-		"name": "Transfer",
-		"type": "event"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "from",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "to",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "transferFrom",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "codinome",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "saTurings",
-				"type": "uint256"
-			}
-		],
-		"name": "vote",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"name": "addressToString",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "owner",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "spender",
-				"type": "address"
-			}
-		],
-		"name": "allowance",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "account",
-				"type": "address"
-			}
-		],
-		"name": "balanceOf",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "decimals",
-		"outputs": [
-			{
-				"internalType": "uint8",
-				"name": "",
-				"type": "uint8"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "name",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"name": "nameToAddress",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "symbol",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "totalSupply",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "votingFinished",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	}
-];
-
-const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
-  provider.send("eth_requestAccounts", []).then(() => {
-    provider.listAccounts().then((accounts) => {
-      const signer = provider.getSigner(accounts[0]);
-
-    /* 3.1 Create instance of Turing smart contract */
-    TuringContract = new ethers.Contract(
-      Turing_Contract_Address,
-      Turing_Contract_ABI,
-      signer
-    );
+  const glassvalue = document.querySelector("#glassvalue") ;
+  const metalvalue = document.querySelector("#metalvalue");
+  const papervalue = document.querySelector("#papervalue");
+  const plasticvalue = document.querySelector("#plasticvalue");
+  const btnGenerat = document.querySelector("#generat");
+  
+  const btnCreatContract = document.querySelector("#factory");
+  const btnInteractionContract = document.querySelector("#interaction");
+  
+  
+  btnInteractionContract.addEventListener("click", () => {
+    divMainGeneration.style.display = 'none';
+    divMainInteraction.style.display = 'flex';
   });
-});
-
-console.log(TuringContract.Address);
-
-
-
-function votar() {
-  let qtdTurings = qtdTuringsInput.value;	
-  var pessoa = SelectPessoa.value;
-  var multiplier = SelectMultiplier.value;
-  let qtdTuringsFinal = qtdTurings * multiplier;
-  //   qtdTurings = str(qtdTuringsFinal);
-
-  TuringContract.vote(pessoa, qtdTuringsFinal.toString()).then(() => {
-    qtdTuringsInput.value = "";
-  }).catch((err) => {
-    alert("Error in voting - " + err.message);
+  
+  btnCreatContract.addEventListener("click", () => {
+    divMainGeneration.style.display = 'flex';
+    divMainInteraction.style.display = 'none';
   });
-
-  console.log(pessoa, qtdTuringsFinal);
-}
-
-voteBtn.addEventListener("click", votar);
-
-function changeMultiplier() {
-  var multiplier = SelectMultiplier.value;
-  qtdTuringsInput.setAttribute("max", (2*10**18) / multiplier);
-}
-
-SelectMultiplier.addEventListener("change", changeMultiplier);
-
-function endVoting() {
-  TuringContract.endVoting().then(() => {
-    alert("Voting ended");
-  }).catch((err) => {
-    alert("Error in ending voting - " + err.message);
-  });
-}
-
-endVotingBtn.addEventListener("click", endVoting);
