236dcff5b25a16a756f314e94337810758ffa68e
vmjesus <vinicius.jesus@edu.ufes.br>
2023-01-29 23:31:19 -0300

    test contract

--- test/SelectiveCollection.js
+++ test/SelectiveCollection.js
@@ -1,4 +1,5 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Selective collection token contract", function () {
   it("Deployment should assign the total supply of tokens to the owner", async function () {
@@ -8,19 +9,19 @@ describe("Selective collection token contract", function () {
 
     const hardhatToken = await Token.deploy();
 
-    const addrCollector1 = await hardhatToken.connect(addr1).createCollectorContract(200,100,0,500);
-    //const addrCollector2 = await hardhatToken.connect(addr2).createCollectorContract(200,100,0,500);
+    await hardhatToken.connect(addr1).createCollectorContract(200,100,0,500);
+    await hardhatToken.connect(addr2).createCollectorContract(200,100,0,500);
 
-    console.log("ADDR1 = ", addr1.address)
-    console.log("ADDR2 = ", addr2.address)
-    console.log("ADDR CONTRACT1 = ", await hardhatToken.collectors(addr1.address));
-    console.log("ADDR CONTRACT2 = ",await hardhatToken.collectors(addr2.address));
-    console.log(addrCollector1);
-    console.log(addrCollector2);
+    console.log("ADDR1 = ", addr1.address);
+    console.log("ADDR2 = ", addr2.address);
+    console.log("ADDR3 = ", await hardhatToken.getCollectorAddress(addr1.address));
+
+    // const collector = ethers.getContractAt("Collector", addrCollector1.address);
+
+    // console.log("glas value = ", await collector.glass_value_per_gram());
 
 
 
-    expect(await hardhatToken.collectors(addr1.address) == 0).to.equal(addrCollector1);
-    expect(await hardhatToken.collectors(addr2.address) == 0).to.equal(addrCollector2);
+    expect(await hardhatToken.collectors(addr1.address) != 0).to.be.true;
   });
 });
